ef656658241e6e417adc857447a826924eb0ef94
gbdsantos0 <41595123+gbdsantos0@users.noreply.github.com>
2018-12-12 15:19:07 -0200

    versão de teste de troca de mensagem udp funcional
    o servidor espera pela mensagem, e o cliente envia

--- Client.py
+++ Client.py
@@ -1,4 +1,36 @@
 import socket
 import threading
+import sys
 
 class Client:
+
+	def __init__(self, port=10000):
+		self.port = port
+		
+
+	def main(self):
+		# Create a UDP socket
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+		server_address = ('localhost', self.port)
+		message = 'This is the message.  It will be repeated.'.encode()
+
+		try:
+
+		    # Send data
+		    print(sys.stderr, 'sending "%s"' % message)
+		    sent = self.sock.sendto(message, server_address)
+
+		    # Receive response
+		    print(sys.stderr, 'waiting to receive')
+		    data, server = self.sock.recvfrom(1024)
+		    print(sys.stderr, 'received "%s"' % data)
+
+		finally:
+		    print(sys.stderr, 'closing socket')
+		    self.sock.close()
+
+
+if __name__ == "__main__":
+	sock=Client()
+	sock.main()
--- Server.py
+++ Server.py
@@ -1,4 +1,33 @@
 import socket
 import threading
+import sys
 
 class Server:
+
+	def __init__(self, port=10000):
+		self.port = port
+		
+
+	def main(self):
+		# Create a TCP/IP socket
+		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		# Bind the socket to the port
+		server_address = ('localhost', self.port)
+		print(sys.stderr, 'starting up on %s port %s' % server_address)
+		self.sock.bind(server_address)
+
+		while True:
+		    print(sys.stderr, '\nwaiting to receive message')
+		    data, address = self.sock.recvfrom(1024)
+		    
+		    print(sys.stderr, 'received %s bytes from %s' % (len(data), address))
+		    print(sys.stderr, data)
+		    
+		    if data:
+		        sent = self.sock.sendto(data, address)
+		        print(sys.stderr, 'sent %s bytes back to %s' % (sent, address))
+
+
+if __name__ == "__main__":
+	sock=Server()
+	sock.main()
