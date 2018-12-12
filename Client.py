e57adedf791557d2707f845507aa385be349a0cf
gbdsantos0 <41595123+gbdsantos0@users.noreply.github.com>
2018-12-12 16:16:36 -0200

    tentativa falha de timeout

--- Client.py
+++ Client.py
@@ -11,24 +11,26 @@ class Client:
 	def main(self):
 		# Create a UDP socket
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
+		# Bind the socket to the port
 		server_address = ('localhost', self.port)
 		message = 'This is the message.  It will be repeated.'.encode()
+		condition = 'y'
+		while condition == 'y':
 
-		try:
+			# Send data
+			print('sending "%s"' % message)
+			sent = self.sock.sendto(message, server_address)
 
-		    # Send data
-		    print(sys.stderr, 'sending "%s"' % message)
-		    sent = self.sock.sendto(message, server_address)
+			# Receive response
+			print('waiting to receive')
+			data, server = self.sock.recvfrom(1024)
+			print('received "%s"' % data)
 
-		    # Receive response
-		    print(sys.stderr, 'waiting to receive')
-		    data, server = self.sock.recvfrom(1024)
-		    print(sys.stderr, 'received "%s"' % data)
-
-		finally:
-		    print(sys.stderr, 'closing socket')
-		    self.sock.close()
+			condition = input("\ncontinue?(y/n) ")
+			print(condition)
+		
+		print('closing socket')
+		self.sock.close()
 
 
 if __name__ == "__main__":
--- Server.py
+++ Server.py
@@ -1,6 +1,7 @@
 import socket
 import threading
 import sys
+import time
 
 class Server:
 
@@ -13,20 +14,33 @@ class Server:
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 		# Bind the socket to the port
 		server_address = ('localhost', self.port)
-		print(sys.stderr, 'starting up on %s port %s' % server_address)
+		print('starting up on %s port %s' % server_address)
 		self.sock.bind(server_address)
-
+		#self.sock.settimeout(1)#Para testes
 		while True:
-		    print(sys.stderr, '\nwaiting to receive message')
-		    data, address = self.sock.recvfrom(1024)
-		    
-		    print(sys.stderr, 'received %s bytes from %s' % (len(data), address))
-		    print(sys.stderr, data)
-		    
-		    if data:
-		        sent = self.sock.sendto(data, address)
-		        print(sys.stderr, 'sent %s bytes back to %s' % (sent, address))
+			print('\nwaiting for clients to connect')
+			initialtime = time.time()
+			curtime = time.time() - initialtime
+			while curtime < 20:
+				try:
+					data, address = self.sock.recvfrom(1024)
+				except (time.time() - curtime+initialtime) > 2: 
+					curtime = time.time() - initialtime
+				finally:
+					curtime = 30
+			
+			if curtime < 30:
+				self.sock.close()
+			else:
+				if data:
+					print('received %s bytes from %s' % (len(data), address))
+					print(data)		
+			
+					sent = self.sock.sendto(data, address)
+					print('sent %s bytes back to %s' % (sent, address))
 
+	def sendPhrase():
+		print('\nfoo')
 
 if __name__ == "__main__":
 	sock=Server()
